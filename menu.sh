b01eabda7c3deb773e3ae4dcc45556adfb23cff0
Ubuntu <ubuntu@ip-172-31-2-242.ap-south-1.compute.internal>
2019-10-15 04:45:31 +0000

    second commit

--- menu.sh
+++ menu.sh
@@ -8,6 +8,21 @@
 #####
 
 
+n=13
+
+for(( i=1; i<=n; i++ ))
+do
+  (( v = $i % 2 )) 
+  if [ $v  != 0 ]
+  then
+    for(( j=1; j<=i; j++ ))
+    do
+      echo -n "#"
+    done
+  fi
+  echo ""
+done
+
 createDir(){
  echo ""
  echo "Enter dir name:"
